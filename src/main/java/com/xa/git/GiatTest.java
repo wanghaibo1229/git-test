dc46679dda0f1c71bae70a5587c528a1b66e9480
whb <2206223752@qq.com>
2023-02-11 14:02:56 +0800

    second commit

--- src/main/java/com/xa/git/GiatTest.java
+++ src/main/java/com/xa/git/GiatTest.java
@@ -3,5 +3,6 @@ package com.xa.git;
 public class GiatTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
