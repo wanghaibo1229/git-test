9140a376893be927cfb8076afc40e8b9b3e3039c
whb <2206223752@qq.com>
2023-02-11 14:12:16 +0800

    hot-fix commit2

--- src/main/java/com/xa/git/GiatTest.java
+++ src/main/java/com/xa/git/GiatTest.java
@@ -6,5 +6,6 @@ public class GiatTest {
         System.out.println("hello git!");
         System.out.println("hello git!3");
         System.out.println("hello git!4");
+        System.out.println("hot-fix test");
     }
 }
