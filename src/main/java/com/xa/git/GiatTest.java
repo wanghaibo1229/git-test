5c9a2de2d3e2a41215b88c22918f52ff96ccdd89
whb <2206223752@qq.com>
2023-02-11 14:26:46 +0800

    push commit

--- src/main/java/com/xa/git/GiatTest.java
+++ src/main/java/com/xa/git/GiatTest.java
@@ -8,5 +8,6 @@ public class GiatTest {
         System.out.println("hello git!4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
